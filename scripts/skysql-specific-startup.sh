751449e45be97cc0c304bb4847a5f97ca6ed49eb
sneJ- <jens.rowekamp@mariadb.com>
2020-09-07 17:20:26 +0000

    DBAAS-4851 - small startup adjustments

--- scripts/skysql-specific-startup.sh
+++ scripts/skysql-specific-startup.sh
@@ -54,12 +54,14 @@ function add-node-to-cluster() {
         curl -X GET -u ${MAXSCALE_API_USERNAME}:${MAXSCALE_API_PASSWORD} ${RELEASE_NAME}-mariadb-maxscale:8989/v1/maxscale --fail 2>/dev/null >/dev/null
     done
 
-    # Add node to MaxScale
     echo ""
-    curl -X GET -u ${MAXSCALE_API_USERNAME}:${MAXSCALE_API_PASSWORD} ${RELEASE_NAME}-mariadb-maxscale:8989/v1/servers/$SHORT_DNS_NAME --fail 2>/dev/null >/dev/null
-    if [ $? -ne 0 ]; then
-        echo "Adding server $SHORT_DNS_NAME to MaxScale"
-        curl -X POST -u ${MAXSCALE_API_USERNAME}:${MAXSCALE_API_PASSWORD} ${RELEASE_NAME}-mariadb-maxscale:8989/v1/servers -d '{"data":{"id":"'$SHORT_DNS_NAME'","type":"servers","attributes":{"parameters":{"address":"'$SHORT_DNS_NAME'","protocol":"MariaDBBackend"}},"relationships":{"services":{"data":[{"id":"Read-Write-Service","type":"services"}]},"monitors":{"data":[{"id":"MariaDB-Monitor","type":"monitors"}]}}}}'
+    if [ $CLUSTER_TOPOLOGY == "columnstore" ]; then
+        # Add node to MaxScale
+        curl -X GET -u ${MAXSCALE_API_USERNAME}:${MAXSCALE_API_PASSWORD} ${RELEASE_NAME}-mariadb-maxscale:8989/v1/servers/$SHORT_DNS_NAME --fail 2>/dev/null >/dev/null
+        if [ $? -ne 0 ]; then
+            echo "Adding server $SHORT_DNS_NAME to MaxScale"
+            curl -X POST -u ${MAXSCALE_API_USERNAME}:${MAXSCALE_API_PASSWORD} ${RELEASE_NAME}-mariadb-maxscale:8989/v1/servers -d '{"data":{"id":"'$SHORT_DNS_NAME'","type":"servers","attributes":{"parameters":{"address":"'$SHORT_DNS_NAME'","protocol":"MariaDBBackend"}},"relationships":{"services":{"data":[{"id":"Read-Write-Service","type":"services"}]},"monitors":{"data":[{"id":"MariaDB-Monitor","type":"monitors"}]}}}}'
+        fi
     fi
 
     touch $SKY_IFLAG
