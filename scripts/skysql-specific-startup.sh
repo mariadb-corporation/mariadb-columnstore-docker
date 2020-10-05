5fdbe31bb9b748cb6811fa8fc17fb0314f94bb5c
sneJ- <jens.rowekamp@mariadb.com>
2020-10-05 16:07:21 +0000

    Fixing the CS and HTAP startup procedure

--- scripts/skysql-specific-startup.sh
+++ scripts/skysql-specific-startup.sh
@@ -80,5 +80,56 @@ if [ ! -e $SKY_IFLAG ]; then
     add-node-to-cluster
 else
     echo "SkySQL init flag exists"
-    curl -s -X PUT https://$PM1_DNS:8640/cmapi/0.4.0/cluster/start --header 'Content-Type:application/json' --header "x-api-key:$CMAPI_KEY" --data '{"timeout":60}' -k | jq .
+    # Check if we are in a single node deployment (HTAP or CS)
+    if [ $NODES_COUNT -eq 1 ]; then
+        echo "Single node deployment detected"
+
+        # Wait for cmapi to be available (unfortunately it isn't directly available after start)
+        echo "Waiting for cmapi to be available"
+        NUM_NODES=$(curl -s https://$PM1_DNS:8640/cmapi/0.4.0/cluster/status --header 'Content-Type:application/json' --header "x-api-key:$CMAPI_KEY" -k --fail | jq .num_nodes -j)
+        while [ $? -ne 0 ] || [ "$NUM_NODES" != "1" ]; do
+            echo -n "."
+            sleep 3
+            NUM_NODES=$(curl -s https://$PM1_DNS:8640/cmapi/0.4.0/cluster/status --header 'Content-Type:application/json' --header "x-api-key:$CMAPI_KEY" -k --fail | jq .num_nodes -j)
+        done
+        
+        # Start the ColumnStore cluster
+        echo "Starting the ColumnStore cluster"
+        curl -s -X PUT https://$PM1_DNS:8640/cmapi/0.4.0/cluster/start --header 'Content-Type:application/json' --header "x-api-key:$CMAPI_KEY" --data '{"timeout":60}' -k | jq .
+
+        # Put the node into read/write mode again
+        echo "Setting the node back into read/write mode"
+        columnstoreDBWrite -c resume
+
+        # In case of HTAP re-add the HTAP UDF
+        if [[ "$CLUSTER_TOPOLOGY" == "htap" ]]; then
+            echo "Re-adding the HTAP UDF"
+            mariadb -e 'CREATE OR REPLACE FUNCTION set_htap_replication RETURNS STRING SONAME "replication.so";'
+            echo "$?"
+        fi
+    else
+        echo "Multi node deployment detected"
+
+        # Wait for cmapi to be available
+        echo "Waiting for cmapi to be available"
+        DBRM_MODE=$(curl -s https://localhost:8640/cmapi/0.4.0/cluster/status --header 'Content-Type:application/json' --header "x-api-key:$CMAPI_KEY" -k --fail | jq .\"$DNS_NAME\".dbrm_mode -j)
+        while [ $? -ne 0 ] || { [ "$DBRM_MODE" != "offline" ] && [ "$DBRM_MODE" != "master" ] && [ "$DBRM_MODE" != "slave" ]; }; do
+            echo -n "."
+            sleep 3
+            DBRM_MODE=$(curl -s https://localhost:8640/cmapi/0.4.0/cluster/status --header 'Content-Type:application/json' --header "x-api-key:$CMAPI_KEY" -k --fail | jq .\"$DNS_NAME\".dbrm_mode -j)
+        done
+
+        # in case the DBRM mode is offline start the cluster
+        if [ "$DBRM_MODE" == 'offline' ]; then
+            echo "Starting the ColumnStore cluster"
+            curl -s -X PUT https://localhost:8640/cmapi/0.4.0/cluster/start --header 'Content-Type:application/json' --header "x-api-key:$CMAPI_KEY" --data '{"timeout":60}' -k | jq .
+        fi
+
+        # Put the node into read/write mode again
+        DBRM_MODE=$(curl -s https://localhost:8640/cmapi/0.4.0/cluster/status --header 'Content-Type:application/json' --header "x-api-key:$CMAPI_KEY" -k --fail | jq .\"$DNS_NAME\".dbrm_mode -j)
+        if [ "$DBRM_MODE" != 'offline' ]; then
+            echo "Setting the node back into read/write mode"
+            columnstoreDBWrite -c resume
+        fi
+    fi
 fi
