c8aeefd72f930a7c501a6fb29e31a41a7a8c95a8
Todd Stoffel <toddstoffel@icloud.com>
2023-05-02 01:17:28 -0700

    Updates

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 function exitColumnStore {
-    monit unmonitor all
     mariadb-admin shutdown
     mcs-stop
-    monit quit
 }
 
 # Clean Remnants
 rm -f /var/run/syslogd.pid
-rm -f /var/run/monit.pid
 rm -f /var/lib/mysql/*.pid
 
 # Start rsyslog
